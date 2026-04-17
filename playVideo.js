43484cb651b84d5a68a3a4f66ae85dc882bf96aa
Daniel Mendoza <danielmendoza1515@gmail.com>
2026-04-17 07:21:32 -0500

    feat: setup inicial — HTML, SCSS, imágenes y assets

---
+++ playVideo.js
@@ -0,0 +1,9 @@
+
+function playVideo (){
+    var video = document.getElementById("artesania");
+    var img = document.getElementById("artImg");
+
+    img.style.display='none';
+    video.style.display='block';
+    video.play();
+}
