cdf9617b6de925b45903bf4620432c594cd10d27
Daniel Mendoza <danielmendoza1515@gmail.com>
2026-04-22 09:31:18 -0500

    style(css): limpieza y estandarización de estilos

--- showMenu.js
+++ showMenu.js
@@ -1,11 +1,6 @@
 function showMenu() {
     var menu = document.getElementsByClassName("menuMobile");
     var boton = document.getElementById("bMenu");
-
-    if (menu[0].classList.contains("active")) {
-        boton.innerText = "≡";
-    } else {
-        boton.innerText = "X";
-    }
     menu[0].classList.toggle("active")
+    boton.classList.toggle("bttActive")
 }
